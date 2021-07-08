9a2a26507a74c065251d4d70b77512cd9eb28ba5
Ken Zou <ken19970723@gmail.com>
2021-07-08 20:59:16 +0800

    修正餐廳座標、名稱被寫死部分 餐廳種類搜尋

--- restaurant/app.js
+++ restaurant/app.js
@@ -14,15 +14,14 @@ app.get('/', (req, res) => {
 // search
 app.get('/search', (req, res) => {
     const keywords = req.query.keyword
-    const results = restaurantList.results.filter(item => item.name.toLowerCase().includes(keywords.toLowerCase()))
-    let noResult
-    results == false ? noResult = '沒有搜尋結果' : noResult = ''
+    const results = restaurantList.results.filter(item => item.name.toLowerCase().includes(keywords.toLowerCase()) || item.category.toLowerCase().includes(keywords.toLowerCase()))
+    let noResult = ''
+    if (results === []) noResult = '沒有搜尋結果'
     res.render('index', { restaurantList: results, keywords, noResult})
 })
 //詳細介紹
 app.get('/restaurants/:id', (req, res) => {
-    let restaurant
-    restaurant = restaurantList.results.find(item => req.params.id === item.id.toString())
+    const restaurant = restaurantList.results.find(item => req.params.id === item.id.toString())
     res.render('show', { restaurant })
 })
 app.listen(port, () => {
