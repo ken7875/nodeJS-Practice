2a66ba467ca55d9665c0871d7c0c0480e84bd0d9
Ken Zou <ken19970723@gmail.com>
2021-07-11 19:47:09 +0800

    create mongodb collection、 create mongodb seeds

--- restaurant-CRUD/app.js
+++ restaurant-CRUD/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
-
+mongoose.connect("mongodb://localhost/restaurant", { useNewUrlParser: true , useUnifiedTopology: true })
 const app = express()
 const port = 3000
+const db = mongoose.connection
+// mongodb連線
+db.once('open', () => {
+    console.log('mongodb connect')
+})
+db.on('error', () => {
+    console.log('connect error!')
+})
 
+app.get('/', (req, res) => {
+    res.send('hello wrold')
+})
 app.listen(port, () => {
     console.log('App is running on http://localhost:3000')
 })
---
+++ restaurant-CRUD/models/seeds/restaurantSeeder.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Restaurant = require('../restaurant');
+const restaurantRes = require('../../restaurant.json');
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection // 將連結賦予到這個變數
+
+db.once('open', () => {
+    console.log('mongodb connect')
+    restaurantRes.results.forEach(restaurant => {
+        Restaurant.create({
+            id: restaurant.id,
+            name: restaurant.name,
+            name_en: restaurant.name_en,
+            image: restaurant.image,
+            catrgory: restaurant.category,
+            location: restaurant.location,
+            phone: restaurant.phone,
+            google_map: restaurant.google_map,
+            rating: restaurant.rating,
+            description: restaurant.description
+        })
+    })
+    console.log('done')
+});
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
